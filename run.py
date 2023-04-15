e1a78476b9e4e01a816980e0d177034d9cd4cc28
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-04-15 21:05:49 +0000

    feat: Add the convert method to the CurrencyConversion class

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ class CurrencyConverter():
     """
     Main class.
     Its first method requests data from the requests library.
+    The second method does the currency conversion and limits the conversion's decimal places to four.
     """
     def __init__(self, url):
         """
@@ -14,3 +15,17 @@ class CurrencyConverter():
         """
         self.data = requests.get(url).json()
         self.currencies = self.data['rates']
+
+    def convert(self, from_currency, to_currency, amount):
+        """
+        This method processes the currency conversion and limits the converted amount to four decimal places.
+        """
+        initial_amount = amount
+        if from_currency != 'USD':
+            amount = amount / self.currencies[from_currency]
+        # limits the decimal places to four
+        amount = round(amount * self.currencies[to_currency], 4)
+        return amount
+        
+api_url = 'https://api.exchangerate-api.com/v4/latest/USD'       
+CurrencyConverter(api_url)
